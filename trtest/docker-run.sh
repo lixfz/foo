0366f9a82a185dfe14777902268429bca1d8a81f
lixfz <lixf@sovon.net>
2025-04-02 10:40:29 +0000

    more outputs

--- trtest/docker-run.sh
+++ trtest/docker-run.sh
@@ -1,11 +1,12 @@
 #docker run -d --name t2 --gpus all -e NVIDIA_VISIBLE_DEVICES=all -v /userdata:/userdata trt:2702 sleep infinity
 #docker run -d --name ta --network host --gpus all -e NVIDIA_VISIBLE_DEVICES=all -v /userdata:/userdata datacanvas/nctest:250328 sleep infinity
-docker run --rm --name ta2 -it \
+docker run --rm --name ta3 -it \
 	--network host \
 	--ipc host \
 	--shm-size=32G \
 	--privileged \
 	--ulimit nofile=65535:65535 \
+	--ulimit stack=655350:655350 \
 	--gpus all -e NVIDIA_VISIBLE_DEVICES=all \
 	-v /userdata:/userdata \
 	trt:250401 \
