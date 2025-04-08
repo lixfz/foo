0f10779188deb8d7247ea5a9fcb8b2459a4c6f8e
lixfz <lixf@sovon.net>
2025-04-08 19:23:11 +0800

    add docs

--- trtest/list_perf.sh
+++ trtest/list_perf.sh
@@ -2,7 +2,17 @@
 
 REPORT_DIR=${1:-.}
 
+if [ "$2" == "ttft" ]; then
+  op=".streaming_metrics.avg_ttft_ms"
+else
+  op=".performance.system_output_throughput_tok_s"
+fi
+
+echo fetch: $op
+echo ""
+
 for f in `ls $REPORT_DIR/*.json`; do
-   r=`jq .performance.system_output_throughput_tok_s  $f`
-   echo $f: $r
+  #r=`jq .performance.system_output_throughput_tok_s  $f`
+  r=`jq $op $f`
+  echo $f: $r
 done
